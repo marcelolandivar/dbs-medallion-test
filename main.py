df3f33be88fe60b446cf729c3a07260889291528
marcelolandivar <marcelolandivar24@gmail.com>
2026-04-08 09:40:28 +0200

    Bundle Deploy

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # =========================
 # main.py
 # =========================
-import os
+import sys
 from pipelines.bronze.load_to_bronze import run_bronze
 from pipelines.silver.load_to_silver import run_silver
 from pipelines.gold.load_to_gold import run_gold
 
 
 if __name__ == "__main__":
-    env_var = os.getenv("ENVIRONMENT")
+    env_var = sys.argv[1] if len(sys.argv) > 1 else "dev"
 
     # Execute layers sequentially
     run_bronze(env_var)
