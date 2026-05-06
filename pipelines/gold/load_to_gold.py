c704f5a93d596af54e07f1d26f61bc1e03470548
marcelolandivar <marcelolandivar24@gmail.com>
2026-05-06 14:34:04 +0200

    Remove df_traffic.show() from load_to_gold.py
    Removed the display of the traffic DataFrame.

--- pipelines/gold/load_to_gold.py
+++ pipelines/gold/load_to_gold.py
@@ -61,7 +61,6 @@ def run_gold(env: str):
 
     df_traffic = read_SilverTrafficTable(spark, cfg)
     df_roads = read_SilverRoadsTable(spark, cfg)
-    df_traffic.show()
     df_vehicle = create_VehicleIntensity(df_traffic)
     df_FinalTraffic = create_LoadTime(df_vehicle)
     df_FinalRoads = create_LoadTime(df_roads)
